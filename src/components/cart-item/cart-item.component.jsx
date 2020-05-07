8a1799d2bd04f15c3d2990b4eeb579cb1cc9f323
Anthony <anthonylandry777@gmail.com>
2020-05-06 18:03:19 -0700

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,10 +5,10 @@ import './cart-item.styles.scss';
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     <div className='cart-item'>
         <img src={imageUrl} alt='item' />
-        <div className='ite-details'>
+        <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>
-                {quantity} * ${price}
+                {quantity} x ${price}
             </span>
         </div>
     </div>
